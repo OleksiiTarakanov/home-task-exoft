b94594fefb2ce47092a5fd2d8a779db4a1e4cfa1
Alex T <siricuster@gmail.com>
2021-03-25 07:57:07 -0400

    updated validators

--- src/app/home-task/form-user/form-user.component.ts
+++ src/app/home-task/form-user/form-user.component.ts
@@ -21,6 +21,8 @@ export class FormUserComponent implements OnInit {
   };
   userForm: FormGroup;
 
+  private readonly passwordRegex: RegExp = /^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}$/;
+
   constructor(private readonly fb: FormBuilder) {
 
   }
@@ -31,26 +33,40 @@ export class FormUserComponent implements OnInit {
       firstName: this.fb.control(this.user.firstName, Validators.required),
       lastName: this.fb.control(this.user.lastName, Validators.required),
       email: this.fb.control(this.user.email, Validators.required),
-      password: this.fb.control(this.user.password, Validators.required),
+      password: this.fb.control(this.user.password, [Validators.required, this.passValidator.bind(this)]),
       confirmPassword: this.fb.control(this.user.confirmPassword, Validators.required),
       status: this.fb.control(this.user.status)
-    })
+    }, { validators: this.confPassValidator })
   }
 
   submit(): void {
-    if (this.userForm.valid){
-      alert(`Hello, ${this.user.firstName}`)
+    if (this.userForm.valid) {
+      this.user = { ...this.user, ...this.userForm.value }
+      alert(
+        `Hello, ${this.user.firstName, this.user.lastName}
+         Username: ${this.user.username}
+         Password: ${this.user.password}
+         email: ${this.user.email}
+         status: ${this.user.status}`
+      )
       console.log(this.userForm)
     } else {
       alert('Form not valid')
     }
   }
 
-  private confPassValidator() {
-    if(this.user.confirmPassword === this.user.password){
-      return true
-    } else {
-      return null
+  private passValidator(control: AbstractControl): ValidationErrors | null {
+    if (control?.value) {
+      const isValid = this.passwordRegex.test(control.value);
+      return !isValid ? { invalidPassword: true } : null;
     }
+
+    return null;
+  }
+
+  private confPassValidator(group: FormGroup) {
+    const pass = group.get('password').value;
+    const confPass = group.get('confirmPassword').value;
+    return pass === confPass ? null : { notSame: true }
   }
 }
